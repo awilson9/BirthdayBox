43097991bd9db9fa8f79f42facf373ceb81671a0
awilson9 <awilson9@wisc.edu>
2016-11-14 17:25:30 -0600

    updates for reminder feature
    fix email script, change style so that it is device scalable

--- php/email-reminder.php
+++ php/email-reminder.php
@@ -3,7 +3,7 @@
 $email_to = $argv[1];
 $email_subject = "Order Reminder From Birthday Box";
 $email_from = "orders@bdayb.com";
-$email_message = "Hello,\n   Thank you for your interest in Birthday Box. This is an order reminder for a birthday on " . $argv[2] . "\nClick on the following link to complete your order: https://bdayb.com/order.html\n\nHave a great day,\n The Birthday Box Team"; 
+$email_message = "Hello,\n\nThank you for your interest in Birthday Box. This is an order reminder for a birthday on " . $argv[2] . "\nClick on the following link to complete your order: https://bdayb.com/order.html\n\nHave a great day,\n The Birthday Box Team"; 
 $headers = 'From: '.$email_from."\r\n".
  
 'Reply-To: '.$email_from."\r\n" .
--- php/reminder.php
+++ php/reminder.php
@@ -7,16 +7,22 @@ if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$email = $_POST['reminder-email'];
-$date = time();
+$email = $_POST['reminder_email'];
+$date = $_POST['reminder_date'];
+$u_date = strtotime($date);
+$send_date_u = $u_date - 604800;
+$dateTime = new DateTime("@$send_date_u");
+$send_date = date_format($dateTime, 'Y-m-d H:i');
 
 $query = "INSERT INTO REMINDERS (REMINDER_EMAIL, REMINDER_BDAY) VALUES ('$email', '$date')";
 if(mysqli_query($conn, $query)){
-	$command = "php email-reminder.php " . $email . " " . $date . " | at " . $date;
-	$output = shell_exec($command);
+	shell_exec('echo php email-reminder.php ' . $email . ' ' .$date . '| at ' . $send_date); 
+	//$command = "cd ../ at " + "2:10 PM" +"\nphp -f email-reminder.php " . $email . " " . $date + "\n^d";
+	//echo shell_exec($command);
 }else{
 	echo "Error: " . $query . "<br>" . mysqli_error($conn);
 }
+
 
 mysqli_close($conn);
 
