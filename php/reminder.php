316a94fce66f4eea8f56d068f42999c61718b631
awilson9 <awilson9@wisc.edu>
2016-11-14 17:51:14 -0600

    Reminder feature updates
    Change styling, fix email bug

--- php/reminder.php
+++ php/reminder.php
@@ -12,11 +12,11 @@ $date = $_POST['reminder_date'];
 $u_date = strtotime($date);
 $send_date_u = $u_date - 604800;
 $dateTime = new DateTime("@$send_date_u");
-$send_date = date_format($dateTime, 'Y-m-d H:i');
+$send_date = date_format($dateTime, 'm/d/Y');
 
 $query = "INSERT INTO REMINDERS (REMINDER_EMAIL, REMINDER_BDAY) VALUES ('$email', '$date')";
 if(mysqli_query($conn, $query)){
-	shell_exec('echo php email-reminder.php ' . $email . ' ' .$date . '| at ' . $send_date); 
+	shell_exec('echo php email-reminder.php ' . $email . ' ' .$date . '| at 10:00 AM ' . $send_date); 
 	//$command = "cd ../ at " + "2:10 PM" +"\nphp -f email-reminder.php " . $email . " " . $date + "\n^d";
 	//echo shell_exec($command);
 }else{
